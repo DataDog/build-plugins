18bd31e714ffbb762f4b4a3c4446abe11d459464
Yoann Moinet <yoann.moinet@datadoghq.com>
2020-05-06 11:01:18 +0200

    Remove dist

--- dist/hooks/datadog/types.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-// Unless explicitly stated otherwise all files in this repository are licensed
-// under the MIT License.
-// This product includes software developed at Datadog (https://www.datadoghq.com/).
-// Copyright 2019-Present Datadog, Inc.
-Object.defineProperty(exports, "__esModule", { value: true });
