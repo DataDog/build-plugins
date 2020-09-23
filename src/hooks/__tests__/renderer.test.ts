be4d77495faa8515924f13bb821297ef297ffee2
Yoann Moinet <yoann.moinet@datadoghq.com>
2020-09-23 10:52:17 +0200

    Update OSS headers

--- src/hooks/__tests__/renderer.test.ts
+++ src/hooks/__tests__/renderer.test.ts
@@ -1,3 +1,7 @@
+// Unless explicitly stated otherwise all files in this repository are licensed under the Apache License Version 2.0.
+// This product includes software developed at Datadog (https://www.datadoghq.com/).
+// Copyright 2019-Present Datadog, Inc.
+
 describe('Renderer', () => {
     test('It should outputGenerals the same with Webpack 5 and 4', () => {
         const { outputGenerals } = require('../renderer');
