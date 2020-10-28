3e2e32de3b7edbfeae6270c44c92a112c2a8dc5e
Yoann Moinet <yoann.moinet@datadoghq.com>
2020-10-28 17:14:41 +0100

    Run yarn oss

--- src/__tests__/mocks/projects/webpack4/workspaces/app/file0000.js
+++ src/__tests__/mocks/projects/webpack4/workspaces/app/file0000.js
@@ -1,3 +1,7 @@
+// Unless explicitly stated otherwise all files in this repository are licensed under the MIT License.
+// This product includes software developed at Datadog (https://www.datadoghq.com/).
+// Copyright 2019-Present Datadog, Inc.
+
 import fn1 from './file0001.js';
 
 const fn = async function () {
