139a6e17c9ada47ad2667ae206b117d18cf9803c
Yoann Moinet <yoann.moinet@datadoghq.com>
2022-01-20 10:32:40 +0100

    Add modules test

--- src/__tests__/esbuild/modules.test.ts
+++ src/__tests__/esbuild/modules.test.ts
@@ -2,21 +2,25 @@
 // This product includes software developed at Datadog (https://www.datadoghq.com/).
 // Copyright 2019-Present Datadog, Inc.
 
-// import { LocalModule } from '../../types';
-// import { getModulesResults } from '../../esbuild/modules';
+import { LocalModule } from '../../types';
+import { getModulesResults } from '../../esbuild/modules';
+import { Metafile } from 'esbuild';
+
+const mockMetaFile: Metafile = {
+    inputs: {
+        module1: {
+            bytes: 1,
+            imports: [],
+        },
+    },
+    outputs: {},
+};
 
 describe('getModulesResults', () => {
     test('It should add module size to the results', () => {
-        // const results = modules.getResults();
-        // for (const module of Object.values(results.modules) as LocalModule[]) {
-        //     expect(module.size).toBeDefined();
-        // }
-    });
-
-    test('It should add chunk names to the results', () => {
-        // const results = modules.getResults();
-        // for (const module of Object.values(results.modules) as LocalModule[]) {
-        //     expect(module.chunkNames).toBeDefined();
-        // }
+        const results = getModulesResults(mockMetaFile, '');
+        for (const module of Object.values(results) as LocalModule[]) {
+            expect(module.size).toBeDefined();
+        }
     });
 });
