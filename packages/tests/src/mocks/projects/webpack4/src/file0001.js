f392f67ba3043d646689d721760ac3acd4f14eed
Yoann Moinet <yoann.moinet@datadoghq.com>
2024-04-25 15:39:36 +0200

    Update mock projects

--- packages/tests/src/mocks/projects/webpack4/src/file0001.js
+++ packages/tests/src/mocks/projects/webpack4/src/file0001.js
@@ -1,9 +1,9 @@
 // Unless explicitly stated otherwise all files in this repository are licensed under the MIT License.
 // This product includes software developed at Datadog (https://www.datadoghq.com/).
 // Copyright 2019-Present Datadog, Inc.
+import fn2 from 'webpack4_app/file0000.js';
 
 import fn1 from './file0000.js';
-import fn2 from 'webpack4_app/file0000.js';
 
 const fn = async function () {
     await fn1();
