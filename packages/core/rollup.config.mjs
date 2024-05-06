c6d2a14402a1514eaf952460f8b69920f2c4026c
Yoann Moinet <yoann.moinet@datadoghq.com>
2024-05-06 11:45:32 +0200

    Update core publish configuration

--- packages/core/rollup.config.mjs
+++ packages/core/rollup.config.mjs
@@ -1,5 +1,17 @@
 import { getDefaultBuildConfigs } from '@dd/tools/rollupConfig.mjs';
+import multi from '@rollup/plugin-multi-entry';
 
 import packageJson from './package.json' assert { type: 'json' };
 
-export default getDefaultBuildConfigs(packageJson);
+export default getDefaultBuildConfigs(packageJson).map((config) => {
+    config.plugins.push(multi({ preserveModules: true }));
+
+    config.input = { include: 'src/*.ts' };
+
+    delete config.output.file;
+    config.output.dir = 'dist/src';
+
+    config.output.preserveModules = true;
+
+    return config;
+});
