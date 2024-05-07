9a2092e50e0a9cb7b93722a9a5c1d54383bd0780
Yoann Moinet <yoann.moinet@datadoghq.com>
2024-05-07 16:25:13 +0200

    Update oss

--- packages/core/src/index.ts
+++ packages/core/src/index.ts
@@ -1,2 +1,6 @@
+// Unless explicitly stated otherwise all files in this repository are licensed under the MIT License.
+// This product includes software developed at Datadog (https://www.datadoghq.com/).
+// Copyright 2019-Present Datadog, Inc.
+
 export * from './helpers';
 export type * from './types';
