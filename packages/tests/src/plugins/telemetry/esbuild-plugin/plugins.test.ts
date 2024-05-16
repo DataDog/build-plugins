f4f4f5d437c2f0d72cfdc3cb064c5a6e78f2dc63
Yoann Moinet <yoann.moinet@datadoghq.com>
2024-05-16 16:31:19 +0200

    oss

--- packages/tests/src/plugins/telemetry/esbuild-plugin/plugins.test.ts
+++ packages/tests/src/plugins/telemetry/esbuild-plugin/plugins.test.ts
@@ -1,6 +1,7 @@
 // Unless explicitly stated otherwise all files in this repository are licensed under the MIT License.
 // This product includes software developed at Datadog (https://www.datadoghq.com/).
 // Copyright 2019-Present Datadog, Inc.
+
 import { wrapPlugins, getResults } from '@dd/telemetry-plugins/esbuild-plugin/plugins';
 import { getMockBuild } from '@dd/tests/testHelpers';
 import type { PluginBuild, Plugin } from 'esbuild';
