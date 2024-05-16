1d31f256da8861c607ccb4f887a7398361ab8ce1
Yoann Moinet <yoann.moinet@datadoghq.com>
2024-05-16 15:17:34 +0200

    Fix templates and injection

--- packages/tools/src/helpers.ts
+++ packages/tools/src/helpers.ts
@@ -13,8 +13,17 @@ if (!process.env.PROJECT_CWD) {
 }
 export const ROOT = process.env.PROJECT_CWD!;
 
+export const IMPORTS_KEY = '/* #imports-injection-placeholder */';
+export const TYPES_KEY = '/* #types-injection-placeholder */';
+export const CONFIGS_KEY = '/* #configs-injection-placeholder */';
+
 const execFileP = promisify(execFile);
 const maxBuffer = 1024 * 1024;
 
 export const execute = (cmd: string, args: string[], cwd: string) =>
     execFileP(cmd, args, { maxBuffer, cwd, encoding: 'utf-8' });
+
+export const injectIntoString = (content: string, mark: string, injection: string) => {
+    const index = content.indexOf(mark) + mark.length;
+    return content.slice(0, index) + injection + content.slice(index);
+};
