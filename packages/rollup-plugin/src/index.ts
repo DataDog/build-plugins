2975c7d0018688191ccb057cc7d49e6d38c7a740
Yoann Moinet <yoann.moinet@datadoghq.com>
2024-07-09 14:25:00 +0200

    Add rollup plugin

---
+++ packages/rollup-plugin/src/index.ts
@@ -0,0 +1,32 @@
+// Unless explicitly stated otherwise all files in this repository are licensed under the MIT License.
+// This product includes software developed at Datadog (https://www.datadoghq.com/).
+// Copyright 2019-Present Datadog, Inc.
+
+// This file is partially generated.
+// Anything between #types-export-injection-marker
+// will be updated using the 'yarn cli integrity' command.
+
+import { buildPluginFactory, helpers } from '@dd/factory';
+
+import pkg from '../package.json';
+
+export const datadogRollupPlugin = buildPluginFactory({
+    version: pkg.version,
+}).rollup;
+
+export { helpers } from '@dd/factory';
+
+export type { Options as RollupPluginOptions } from '@dd/core/types';
+
+export type {
+    // #types-export-injection-marker
+    RumTypes,
+    TelemetryTypes,
+    // #types-export-injection-marker
+} from '@dd/factory';
+
+// This is to prevent overrides from other libraries in the final bundle.
+module.exports = {
+    helpers,
+    datadogRollupPlugin,
+};
