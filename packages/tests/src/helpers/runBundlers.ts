7ebb7dcf1bbae3d00019b3bab4137a8b22831548
Yoann Moinet <yoann.moinet@datadoghq.com>
2024-07-17 15:02:02 +0200

    Integrity

--- packages/tests/src/helpers/runBundlers.ts
+++ packages/tests/src/helpers/runBundlers.ts
@@ -1,6 +1,7 @@
 // Unless explicitly stated otherwise all files in this repository are licensed under the MIT License.
 // This product includes software developed at Datadog (https://www.datadoghq.com/).
 // Copyright 2019-Present Datadog, Inc.
+
 import type { Options } from '@dd/core/types';
 import type { BuildOptions } from 'esbuild';
 import { rmSync } from 'fs';
