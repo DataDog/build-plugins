84dd0c4a88a47fa692121b45c2aaada41bb06f74
Yoann Moinet <yoann.moinet@datadoghq.com>
2024-09-11 15:51:20 +0200

    Integrity

--- packages/tests/src/core/plugins/build-report/esbuild.test.ts
+++ packages/tests/src/core/plugins/build-report/esbuild.test.ts
@@ -1,3 +1,7 @@
+// Unless explicitly stated otherwise all files in this repository are licensed under the MIT License.
+// This product includes software developed at Datadog (https://www.datadoghq.com/).
+// Copyright 2019-Present Datadog, Inc.
+
 import { getEntryNames } from '@dd/core/plugins/build-report/esbuild';
 import { getContextMock } from '@dd/tests/helpers/mocks';
 import { vol } from 'memfs';
