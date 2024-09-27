bf812f9ac914aa6182e38c796473ff94690b3df1
Yoann Moinet <yoann.moinet@datadoghq.com>
2024-09-27 10:48:58 +0200

    Integrity

--- packages/tests/src/plugins/rum/sourcemaps/sender.test.ts
+++ packages/tests/src/plugins/rum/sourcemaps/sender.test.ts
@@ -1,6 +1,7 @@
 // Unless explicitly stated otherwise all files in this repository are licensed under the MIT License.
 // This product includes software developed at Datadog (https://www.datadoghq.com/).
 // Copyright 2019-Present Datadog, Inc.
+
 import { doRequest } from '@dd/core/helpers';
 import { getData, sendSourcemaps, upload } from '@dd/rum-plugins/sourcemaps/sender';
 import { getContextMock } from '@dd/tests/helpers/mocks';
