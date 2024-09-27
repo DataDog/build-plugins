6df31408e7c950a4e8300f433d53d24bf854f418
Yoann Moinet <yoann.moinet@datadoghq.com>
2024-09-27 16:49:44 +0200

    Better logging in tests

--- packages/tests/src/setupTests.ts
+++ packages/tests/src/setupTests.ts
@@ -2,10 +2,12 @@
 // This product includes software developed at Datadog (https://www.datadoghq.com/).
 // Copyright 2019-Present Datadog, Inc.
 
+import console from 'console';
 import nock from 'nock';
 
 import { toBeWithinRange } from './helpers/toBeWithinRange.ts';
 
+// Extend Jest's expect with custom matchers.
 expect.extend({
     toBeWithinRange,
 });
@@ -14,12 +16,9 @@ declare module 'expect' {
     interface AsymmetricMatchers {
         toBeWithinRange(floor: number, ceiling: number): void;
     }
-    interface Matchers<R> {
-        toBeWithinRange(floor: number, ceiling: number): R;
-    }
-}
 
-global.beforeAll(() => {
     // Do not send any HTTP requests.
     nock.disableNetConnect();
-});
+
+// Have a simpler, less verbose, console.log output.
+global.console = console;
