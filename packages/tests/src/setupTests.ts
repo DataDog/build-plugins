3c65bc1f8ef8d92fcc727ed5098826cc3824ea73
Yoann Moinet <yoann.moinet@datadoghq.com>
2024-09-27 16:50:15 +0200

    Add toRepeatString matchers to jest

--- packages/tests/src/setupTests.ts
+++ packages/tests/src/setupTests.ts
@@ -6,19 +6,39 @@ import console from 'console';
 import nock from 'nock';
 
 import { toBeWithinRange } from './helpers/toBeWithinRange.ts';
+import { toRepeatStringRange } from './helpers/toRepeatStringRange.ts';
+import { toRepeatStringTimes } from './helpers/toRepeatStringTimes.ts';
 
 // Extend Jest's expect with custom matchers.
 expect.extend({
     toBeWithinRange,
+    toRepeatStringTimes,
+    toRepeatStringRange,
 });
 
-declare module 'expect' {
-    interface AsymmetricMatchers {
-        toBeWithinRange(floor: number, ceiling: number): void;
-    }
+interface CustomMatchers<R> {
+    toBeWithinRange(floor: number, ceiling: number): R;
+    toRepeatStringTimes(st: string, occurences: number): R;
+    toRepeatStringRange(st: string, range: [number, number]): R;
+}
 
-    // Do not send any HTTP requests.
-    nock.disableNetConnect();
+interface NonCustomMatchers {
+    toBeWithinRange(floor: number, ceiling: number): number;
+    toRepeatStringTimes(st: string, occurences: number): string;
+    toRepeatStringRange(st: string, range: [number, number]): string;
+}
+
+declare global {
+    namespace jest {
+        interface Expect extends NonCustomMatchers {}
+        interface Matchers<R> extends CustomMatchers<R> {}
+        interface InverseAsymmetricMatchers extends NonCustomMatchers {}
+        interface AsymmetricMatchers extends NonCustomMatchers {}
+    }
+}
+
+// Do not send any HTTP requests.
+nock.disableNetConnect();
 
 // Have a simpler, less verbose, console.log output.
 global.console = console;
