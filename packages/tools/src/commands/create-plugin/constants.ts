e628a0f342f835092559412f3d6b4b1fda537080
Yoann Moinet <yoann.moinet@datadoghq.com>
2024-10-03 11:06:50 +0200

    Integrity

--- packages/tools/src/commands/create-plugin/constants.ts
+++ packages/tools/src/commands/create-plugin/constants.ts
@@ -1,3 +1,7 @@
+// Unless explicitly stated otherwise all files in this repository are licensed under the MIT License.
+// This product includes software developed at Datadog (https://www.datadoghq.com/).
+// Copyright 2019-Present Datadog, Inc.
+
 export const typesOfPlugin = ['universal', 'bundler'] as const;
 
 export const bundlerHooks = ['webpack', 'esbuild', 'vite', 'rollup', 'rspack', 'farm'] as const;
