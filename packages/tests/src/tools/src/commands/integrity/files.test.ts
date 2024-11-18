73d460a5ff3334c384ce8a4dae92ada792fe6297
Yoann Moinet <yoann.moinet@datadoghq.com>
2024-11-18 09:22:02 -0500

    Better params parser

---
+++ packages/tests/src/tools/src/commands/integrity/files.test.ts
@@ -0,0 +1,46 @@
+import { parseParams } from '@dd/tools/commands/integrity/files';
+
+describe('files.ts', () => {
+    describe('parseParams', () => {
+        const expectations = [
+            {
+                st: `function named (arg1) {
+  console.log("Some content");
+}`,
+                expected: ['arg1'],
+                description: 'basic named function with 1 argument',
+            },
+            {
+                st: `function named ( arg1, arg2 ) {
+    console.log("Some content");
+}`,
+                expected: ['arg1', 'arg2'],
+                description: 'basic named function with 2 arguments and spaces',
+            },
+            {
+                st: `const myFn = ( arg1 , arg2 ) => {
+    console.log("Some content");
+};`,
+                expected: ['arg1', 'arg2'],
+                description: 'basic arrow function with 2 arguments and spaces',
+            },
+            {
+                st: `function weird (
+    arg1,
+    arg2, // Some comment.
+// Some other comment.
+    arg3,
+) {
+    console.log("Some content");
+}`,
+                expected: ['arg1', 'arg2', 'arg3'],
+                description: 'weird named function with comments and spaces',
+            },
+        ];
+
+        test.each(expectations)('Should parse $description.', ({ st, expected }) => {
+            const result = parseParams(st);
+            expect(result).toEqual(expected);
+        });
+    });
+});
