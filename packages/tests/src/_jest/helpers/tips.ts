5ff850b289176d29d470a624eae542562c16df46
Yoann Moinet <yoann.moinet@datadoghq.com>
2024-11-26 15:59:32 +0100

    Integrity

--- packages/tests/src/_jest/helpers/tips.ts
+++ packages/tests/src/_jest/helpers/tips.ts
@@ -1,3 +1,7 @@
+// Unless explicitly stated otherwise all files in this repository are licensed under the MIT License.
+// This product includes software developed at Datadog (https://www.datadoghq.com/).
+// Copyright 2019-Present Datadog, Inc.
+
 import { FULL_NAME_BUNDLERS } from '@dd/core/constants';
 import type { BundlerFullName } from '@dd/core/types';
 import { bgYellow, dim, green, red } from '@dd/tools/helpers';
