f3f0a9a6d49e80d7fa9da1e1aef598f25d687ead
Yoann Moinet <yoann.moinet@datadoghq.com>
2024-12-03 18:04:19 +0100

    Integrity and move devDependency

--- packages/tools/src/build/rumReactPlugin.ts
+++ packages/tools/src/build/rumReactPlugin.ts
@@ -1,3 +1,7 @@
+// Unless explicitly stated otherwise all files in this repository are licensed under the MIT License.
+// This product includes software developed at Datadog (https://www.datadoghq.com/).
+// Copyright 2019-Present Datadog, Inc.
+
 import { createBrowserRouter } from '@datadog/browser-rum-react/react-router-v6';
 import { reactPlugin } from '@datadog/browser-rum-react';
 
