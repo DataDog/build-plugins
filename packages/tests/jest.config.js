9504f75d968e8c40f0ed816aaeaa3e9a30ab0506
Yoann Moinet <yoann.moinet@datadoghq.com>
2025-01-16 10:05:09 +0100

    Small test inconsequential update

--- packages/tests/jest.config.js
+++ packages/tests/jest.config.js
@@ -7,13 +7,10 @@ module.exports = {
     clearMocks: true,
     globalSetup: '<rootDir>/src/_jest/globalSetup.ts',
     preset: 'ts-jest/presets/js-with-ts',
-    reporters: [['default', { summaryThreshold: 2 }]],
     // Without it, vite import is silently crashing the process with code SIGHUP 129
     resetModules: true,
     roots: ['./src/'],
     setupFilesAfterEnv: ['<rootDir>/src/_jest/setupAfterEnv.ts'],
     testEnvironment: 'node',
     testMatch: ['**/*.test.*'],
-    // We're building a lot of projects in parallel, so we need to increase the timeout.
-    testTimeout: 20000,
 };
