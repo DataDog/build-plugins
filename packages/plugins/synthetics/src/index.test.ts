0ab5804f56c743e37e5abea58d87b45af4c3d13a
Yoann Moinet <yoann.moinet@datadoghq.com>
2025-03-26 10:56:00 +0100

    Add tests

--- packages/plugins/synthetics/src/index.test.ts
+++ packages/plugins/synthetics/src/index.test.ts
@@ -2,9 +2,21 @@
 // This product includes software developed at Datadog (https://www.datadoghq.com/).
 // Copyright 2019-Present Datadog, Inc.
 
+import { runServer } from '@dd/core/helpers/server';
+import { API_PREFIX, DEFAULT_PORT } from '@dd/synthetics-plugin/constants';
 import { getPlugins } from '@dd/synthetics-plugin';
 import { getContextMock } from '@dd/tests/_jest/helpers/mocks';
-import { runBundlers } from '@dd/tests/_jest/helpers/runBundlers';
+import nock from 'nock';
+
+jest.mock('@dd/core/helpers/server', () => {
+    const original = jest.requireActual('@dd/core/helpers/server');
+    return {
+        ...original,
+        runServer: jest.fn(original.runServer),
+    };
+});
+
+const runServerMocked = jest.mocked(runServer);
 
 describe('Synthetics Plugin', () => {
     describe('getPlugins', () => {
@@ -22,9 +34,77 @@ describe('Synthetics Plugin', () => {
         });
     });
 
-    test('Should run the server at the end of the build.', async () => {
-        await runBundlers({
-            logLevel: 'debug',
+    describe('Server', () => {
+        beforeAll(() => {
+            // Allow local server.
+            nock.enableNetConnect('127.0.0.1');
+        });
+
+        afterEach(async () => {
+            // Kill the server.
+            try {
+                await fetch(`http://127.0.0.1:${DEFAULT_PORT}/${API_PREFIX}/kill`);
+            } catch (e) {
+                // Do nothing.
+            }
+        });
+
+        afterAll(() => {
+            nock.cleanAll();
+            nock.disableNetConnect();
+        });
+
+        describe('to run or not to run', () => {
+            afterEach(() => {
+                // Remove the variables we've set.
+                delete process.env.BUILD_PLUGINS_S8S_LOCAL;
+                delete process.env.BUILD_PLUGINS_S8S_PORT;
+            });
+            const expectations = [
+                {
+                    description: 'not run with no variables',
+                    env: {},
+                    shouldRun: false,
+                },
+                {
+                    description: 'not run with missing port',
+                    env: {
+                        BUILD_PLUGINS_S8S_LOCAL: '1',
+                    },
+                    shouldRun: false,
+                },
+                {
+                    description: 'not run with missing local',
+                    env: {
+                        BUILD_PLUGINS_S8S_PORT: JSON.stringify(DEFAULT_PORT),
+                    },
+                    shouldRun: false,
+                },
+                {
+                    description: 'run with both variables',
+                    env: {
+                        BUILD_PLUGINS_S8S_PORT: JSON.stringify(DEFAULT_PORT),
+                        BUILD_PLUGINS_S8S_LOCAL: '1',
+                    },
+                    shouldRun: true,
+                },
+            ];
+
+            test.each(expectations)(
+                'Should $description.',
+                async ({ description, env, shouldRun }) => {
+                    // Set the variables.
+                    Object.assign(process.env, env);
+                    // Run the plugin.
+                    getPlugins({}, getContextMock());
+                    // Check the server.
+                    if (shouldRun) {
+                        expect(runServerMocked).toHaveBeenCalled();
+                    } else {
+                        expect(runServerMocked).not.toHaveBeenCalled();
+                    }
+                },
+            );
         });
     });
 });
