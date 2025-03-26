d0b449089e0d8b5c88cc7b1582443fc70e8f2284
Yoann Moinet <yoann.moinet@datadoghq.com>
2025-03-26 17:59:28 +0100

    Add missing OSS header

--- packages/plugins/synthetics/src/validate.ts
+++ packages/plugins/synthetics/src/validate.ts
@@ -1,3 +1,7 @@
+// Unless explicitly stated otherwise all files in this repository are licensed under the MIT License.
+// This product includes software developed at Datadog (https://www.datadoghq.com/).
+// Copyright 2019-Present Datadog, Inc.
+
 import type { Options, Logger, GlobalContext } from '@dd/core/types';
 import { CONFIG_KEY, DEFAULT_PORT } from '@dd/synthetics-plugin/constants';
 import type { SyntheticsOptionsWithDefaults } from '@dd/synthetics-plugin/types';
