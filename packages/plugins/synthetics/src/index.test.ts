0cc8f29fe32e70a3e83a96a23d938841d756ca31
Yoann Moinet <yoann.moinet@datadoghq.com>
2025-04-04 17:18:05 +0200

    Missing new line

--- packages/plugins/synthetics/src/index.test.ts
+++ packages/plugins/synthetics/src/index.test.ts
@@ -1,6 +1,7 @@
 // Unless explicitly stated otherwise all files in this repository are licensed under the MIT License.
 // This product includes software developed at Datadog (https://www.datadoghq.com/).
 // Copyright 2019-Present Datadog, Inc.
+
 import { runServer } from '@dd/core/helpers/server';
 import { API_PREFIX } from '@dd/synthetics-plugin/constants';
 import type { ServerResponse } from '@dd/synthetics-plugin/types';
