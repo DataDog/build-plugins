9c08cf58a7b92341dc13f17031aaa4fd92fbdd9a
Yoann Moinet <yoann.moinet@datadoghq.com>
2025-05-07 11:55:37 +0200

    Revert "Use tapAsync instead of tapPromise on shutdown hook"
    This reverts commit 198c6e38e1b0e3c855e305a985902ba9fdcbf9f7.

--- packages/plugins/true-end/src/index.ts
+++ packages/plugins/true-end/src/index.ts
@@ -22,11 +22,7 @@ export const getTrueEndPlugins: GetInternalPlugins = (arg: GetPluginsArg) => {
     const xpackPlugin: PluginOptions['rspack'] & PluginOptions['webpack'] = (compiler) => {
         if (compiler.hooks.shutdown) {
             // NOTE: rspack prior to 1.2.* will randomly crash on shutdown.tapPromise.
-            compiler.hooks.shutdown.tapAsync(PLUGIN_NAME, (callback) => {
-                bothHookFns().then(() => {
-                    callback();
-                });
-            });
+            compiler.hooks.shutdown.tapPromise(PLUGIN_NAME, bothHookFns);
         } else {
             // Webpack 4 only.
             compiler.hooks.done.tapPromise(PLUGIN_NAME, bothHookFns);
