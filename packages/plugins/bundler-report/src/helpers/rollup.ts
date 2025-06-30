7e37ce557baa02d60d34c95e0f8ce1fc5b511be8
Yoann Moinet <yoann.moinet@datadoghq.com>
2025-06-30 16:32:24 +0200

    Integrity

--- packages/plugins/bundler-report/src/helpers/rollup.ts
+++ packages/plugins/bundler-report/src/helpers/rollup.ts
@@ -1,3 +1,7 @@
+// Unless explicitly stated otherwise all files in this repository are licensed under the MIT License.
+// This product includes software developed at Datadog (https://www.datadoghq.com/).
+// Copyright 2019-Present Datadog, Inc.
+
 import { getHighestPackageJsonDir, getNearestCommonDirectory } from '@dd/core/helpers/paths';
 import path from 'path';
 import type { InputOptions, OutputOptions, RollupOptions } from 'rollup';
