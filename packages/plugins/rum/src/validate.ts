34c0586612ef43cb9585d2c71eccad1c9c62bd9f
Yoann Moinet <yoann.moinet@datadoghq.com>
2026-02-02 11:30:15 -0500

    Fix lint

--- packages/plugins/rum/src/validate.ts
+++ packages/plugins/rum/src/validate.ts
@@ -1,8 +1,8 @@
-import type { Site } from '@datadog/browser-rum';
 // Unless explicitly stated otherwise all files in this repository are licensed under the MIT License.
 // This product includes software developed at Datadog (https://www.datadoghq.com/).
 // Copyright 2019-Present Datadog, Inc.
 
+import type { Site } from '@datadog/browser-rum';
 import type { Logger, Options, OptionsWithDefaults } from '@dd/core/types';
 import chalk from 'chalk';
 
