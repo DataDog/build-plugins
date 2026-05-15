880dcce342faf2c3ce05c5ff4eeb264c120989b9
Yoann Moinet <yoann.moinet@datadoghq.com>
2026-05-15 14:45:58 -0400

    Clean RUM Browser SDK types

--- packages/plugins/rum/src/types.ts
+++ packages/plugins/rum/src/types.ts
@@ -2,12 +2,9 @@
 // This product includes software developed at Datadog (https://www.datadoghq.com/).
 // Copyright 2019-Present Datadog, Inc.
 
-import type {
-    datadogRum,
-    RumInitConfiguration as ExpRumInitConfiguration,
-} from '@datadog/browser-rum';
 import type { Assign } from '@dd/core/types';
 
+import type { RumInitConfiguration } from './browserSdkTypes';
 import type { PrivacyOptions, PrivacyOptionsWithDefaults } from './privacy/types';
 
 export type SourceCodeContextOptions = {
@@ -21,9 +18,6 @@ export type RumOptions = {
     privacy?: PrivacyOptions;
     sourceCodeContext?: SourceCodeContextOptions;
 };
-
-export type RumPublicApi = typeof datadogRum;
-export type RumInitConfiguration = ExpRumInitConfiguration;
 
 export type SDKOptions = Assign<
     RumInitConfiguration,
